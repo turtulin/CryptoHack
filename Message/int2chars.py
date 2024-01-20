9ca047be317876e83b6b272d218019af98d1e72d
turtulin <marta.musso@studenti.unicam.it>
2024-01-20 18:41:56 +0100

    [add] Added ex long_to_bytes

--- Message/int2chars.py
+++ Message/int2chars.py
@@ -1,4 +1,5 @@
+from Crypto.Util.number import *
 
 dec = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
-hx = hex(dec)
-print(hx)
+dec_bytes = long_to_bytes(dec)
+print(dec_bytes)
